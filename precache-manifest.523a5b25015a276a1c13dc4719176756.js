dd2494b7d84b551991cd52851845f0168b3f282f
温明祥 <2442845618@qq.com>
2020-08-25 11:31:44 +0800

    first commit

---
+++ precache-manifest.523a5b25015a276a1c13dc4719176756.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad869024dce323d78e8119edf56f1b5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c0fd4abb0a018ff4c0c",
+    "url": "./static/css/2.71c0572d.chunk.css"
+  },
+  {
+    "revision": "cb198e39cdda61c2bc5c",
+    "url": "./static/css/main.d866cee2.chunk.css"
+  },
+  {
+    "revision": "5c0fd4abb0a018ff4c0c",
+    "url": "./static/js/2.f237e7c9.chunk.js"
+  },
+  {
+    "revision": "005ca1ef3dc950b5419a2ae80bec3ca3",
+    "url": "./static/js/2.f237e7c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb198e39cdda61c2bc5c",
+    "url": "./static/js/main.77695296.chunk.js"
+  },
+  {
+    "revision": "851979e7cd8dd72956f9",
+    "url": "./static/js/runtime-main.b7208cbd.js"
+  }
+]);
